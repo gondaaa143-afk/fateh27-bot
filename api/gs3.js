642476329dfe102d553b70244baf7c6f3fa43e72
gondaaa143-afk <gondaaa143@gmail.com>
2026-09-11 17:07:57 +0530

    Update gs3.js

--- api/gs3.js
+++ api/gs3.js
@@ -1,6 +1,5 @@
+import { PYQS } from "./pyqsData.js";
+
 export default function handler(req,res){
- res.status(200).json([
-  {year:2026,question:"Discuss India's energy transition strategy."},
-  {year:2025,question:"Examine the challenges of food security."}
- ]);
+  res.status(200).json(PYQS.filter(q=>q.paper==="GS3"));
 }
