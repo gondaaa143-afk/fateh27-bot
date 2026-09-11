1c688626b2e792797dae39eab1559c7daeb7329b
gondaaa143-afk <gondaaa143@gmail.com>
2026-09-11 15:50:52 +0530

    Create saveCurrent.js

---
+++ api/saveCurrent.js
@@ -0,0 +1,12 @@
+export default async function handler(req,res){
+
+ if(req.method!=="POST"){
+   return res.status(405).json({message:"Method Not Allowed"});
+ }
+
+ return res.status(200).json({
+   success:true,
+   message:"Current Affairs Saved (Demo)"
+ });
+
+}
