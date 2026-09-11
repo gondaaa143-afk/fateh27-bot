37136674c2aa4c1ad1c01d97e17d1512a3a281a7
gondaaa143-afk <gondaaa143@gmail.com>
2026-09-11 15:52:19 +0530

    Create currentData.js

---
+++ api/currentData.js
@@ -0,0 +1,9 @@
+export const currentAffairs = [
+  {
+    id: 1,
+    title: "Mission Mausam",
+    source: "PIB",
+    date: "11 Sept 2026",
+    summary: "Mission Mausam aims to improve weather forecasting and climate resilience."
+  }
+];
