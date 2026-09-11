0af33395da95bef4c7f940f821c8bf137f4b9827
gondaaa143-afk <gondaaa143@gmail.com>
2026-09-11 17:07:20 +0530

    Update gs2.js

--- api/gs2.js
+++ api/gs2.js
@@ -1,12 +1,5 @@
+import { PYQS } from "./pyqsData.js";
+
 export default function handler(req,res){
- res.status(200).json([
-  {
-   year:2026,
-   question:"Discuss the role of cooperative federalism."
-  },
-  {
-   year:2025,
-   question:"Examine the importance of local governance."
-  }
- ]);
+  res.status(200).json(PYQS.filter(q=>q.paper==="GS2"));
 }
