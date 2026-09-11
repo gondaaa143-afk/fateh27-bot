9fc04347709b02ca77df27c2e62eb0a21d3239e1
gondaaa143-afk <gondaaa143@gmail.com>
2026-09-11 15:30:52 +0530

    Create gs4.js

---
+++ api/gs4.js
@@ -0,0 +1,6 @@
+export default function handler(req,res){
+ res.status(200).json([
+  {year:2026,question:"Discuss the importance of integrity in public service."},
+  {year:2025,question:"Differentiate between ethics and values with examples."}
+ ]);
+}
